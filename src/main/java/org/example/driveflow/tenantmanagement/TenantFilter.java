da520fa10ac6e20f539a83d8fb71f233cdeb0522
zakariaal-jumaei <zakariaalsarah2018@gmail.com>
2026-07-14 14:52:52 +0200

    implement TenantFilter #13

--- src/main/java/org/example/driveflow/tenantmanagement/TenantFilter.java
+++ src/main/java/org/example/driveflow/tenantmanagement/TenantFilter.java
@@ -1,4 +1,42 @@
 package org.example.driveflow.tenantmanagement;
 
-public class TenantFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class TenantFilter extends OncePerRequestFilter {
+
+    private static final String TENANT_HEADER_NAME = "X-Tenant-Id";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String tenantIdHeader = resolveTenantId(request);
+        if (tenantIdHeader == null || tenantIdHeader.isBlank()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\":\"Tenant Id is missing\"}");
+            return;
+        }
+        try{
+            long tennatId = Long.parseLong(tenantIdHeader);
+            TenantContext.setCurrentTenant(tennatId);
+            filterChain.doFilter(request, response);
+        }finally {
+            TenantContext.removeCurrentTenant();
+        }
+
+    }
+
+    private String resolveTenantId(HttpServletRequest request) {
+        String tenantIdHeader = request.getHeader(TENANT_HEADER_NAME);
+        if (tenantIdHeader != null && !tenantIdHeader.isBlank()) {
+            return tenantIdHeader;
+        }
+        return null;
+    }
 }
